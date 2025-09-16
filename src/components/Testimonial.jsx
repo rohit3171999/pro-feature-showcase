ff60c129c5d30c3a45066e5d7be02f4a9d598955
rohit3171999 <rohit3171999@gmail.com>
2025-09-16 17:53:28 +0530

    add functional component in Testimonial.jsx

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,27 @@
+/*
+* Description:
+Contains customer quotes and real-world use cases.
+
+Purpose:
+Build credibility and social proof.
+* */
+export default function Testimonial(){
+    return (
+        <>
+            {/*<section>*/}
+            {/*    <h2>Section Title</h2>*/}
+            {/*    <ul>*/}
+            {/*        <li>*/}
+            {/*            <blockquote>Customer quote</blockquote>*/}
+            {/*            <cite>Customer name & role</cite>*/}
+            {/*        </li>*/}
+            {/*        <li>...other testimonials...</li>*/}
+            {/*    </ul>*/}
+            {/*    <div>*/}
+            {/*        <h3>Use Case Title</h3>*/}
+            {/*        <p>Use case description</p>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+        </>
+    );
+}
