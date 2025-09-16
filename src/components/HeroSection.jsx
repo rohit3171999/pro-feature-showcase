bf60cbc3184831a5e32baf473f2b083913798547
rohit3171999 <rohit3171999@gmail.com>
2025-09-16 17:23:37 +0530

    add functional component in HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,17 @@
+/*
+* - Generate a sitemap for this React web application
+- Generate a detailed list of custom components for this React web application
+- For each of the custom component detail the description and purpose of the custom component and include a list of HTML elements required inside the custom components
+* */
 export default function HeroSection(){
     return(
-        <></>
+        <section>
+            {/*<h1>Headline</h1>*/}
+            {/*<p>Subheadline</p>*/}
+            {/*<button>Primary CTA</button>*/}
+            {/*<button>Secondary CTA (optional)</button>*/}
+            {/*<img /><!-- Screenshot or animation -->*/}
+        </section>
+
     )
 }
