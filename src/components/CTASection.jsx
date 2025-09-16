455020d103d7beef9fd8e518769c8a4f3bbd694b
rohit3171999 <rohit3171999@gmail.com>
2025-09-16 17:56:46 +0530

    add functional component in CTASection.jsx

---
+++ src/components/CTASection.jsx
@@ -0,0 +1,19 @@
+/*
+* Description:
+Reinforces call-to-action at the bottom of the page.
+
+Purpose:
+Give users another chance to take action after seeing all details.
+* */
+export default function CTASection(){
+    return (
+        <>
+            {/*<section>*/}
+            {/*    <h2>Call to Action Heading</h2>*/}
+            {/*    <p>Encouraging text</p>*/}
+            {/*    <button>Primary CTA</button>*/}
+            {/*    <button>Secondary CTA</button>*/}
+            {/*</section>*/}
+        </>
+    );
+}
